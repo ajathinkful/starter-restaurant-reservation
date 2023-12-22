034badd72c9fbb6f47ba24f4cde3631b7888e0c4
Alex <aja44@pitt.edu>
2023-12-22 16:55:24 -0500

    us 1-6 passing 7 8 backend passing

--- back-end/src/reservations/reservations.router.js
+++ back-end/src/reservations/reservations.router.js
@@ -3,9 +3,19 @@
 const router = require("express").Router();
 const controller = require("./reservations.controller");
 
-router.route("/search").get(controller.search);
-
-router.route("/").get(controller.list).post(controller.create);
+router
+  .route("/")
+  .get((req, res, next) => {
+    // Check if the mobile_number query parameter exists
+    if (req.query.mobile_number) {
+      // If it exists, call the middleware for the search controller
+      controller.search[0](req, res, next);
+    } else {
+      // If it doesn't exist, call the middleware for the list controller
+      controller.list[0](req, res, next);
+    }
+  })
+  .post(controller.create);
 
 router
   .route("/:reservation_id")
